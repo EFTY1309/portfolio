ec4f8ccf2b296d0659e9b85776b6019e74feb4d5
efty <efty3222@gmail.com>
2024-01-26 22:51:53 +0600

    before copying navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,8 @@ import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
+  
 
   return (
     <nav
@@ -34,7 +36,8 @@ const Navbar = () => {
                 active===Link.title
                 ? "text-white"
                 : "text-secondary"
-               }`}
+               } hover:text-white text-[18px] font-medium cursor-pointer` }
+               onClick={()=> setActive(Link.title)}
              >
               <a href={`#${Link.id}`}>{Link.title}</a>
              </li>
@@ -44,10 +47,21 @@ const Navbar = () => {
            }
 
         </ul>
+        
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+         <img src={toggle? close :menu} alt="menu" className="w-[28px] h-[28px] object-contain cursor-pointer"
+           onClick={()=>setToggle(!toggle)}
+         />
+         <div>
+
+          
+         </div>
+
+        </div>
       </div>
     </nav>
   );
 };
 
 export default Navbar;
-hello the boy
+
